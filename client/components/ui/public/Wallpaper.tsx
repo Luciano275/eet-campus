44ab5da7b105f33a3d3c3a650ab1f9172ba39e56
Luciano275 <luna800153@gmail.com>
2024-06-27 22:09:40 -0300

    Quitando el wallpaper

--- client/components/ui/public/Wallpaper.tsx
+++ client/components/ui/public/Wallpaper.tsx
@@ -1,12 +1,8 @@
+import bgStyles from '@/styles/bg.module.css'
+
 export default function Wallpaper() {
     return (
-        <div className="absolute top-0 left-0 -z-50 w-full h-screen overflow-hidden" style={{
-            backgroundImage: 'url(/wp-min.jpg)',
-            backgroundAttachment: 'fixed',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover'
-        }}>
+        <div className="absolute top-0 left-0 -z-50 w-full h-screen overflow-hidden" id={bgStyles.wallpaper}>
             <div className="absolute top-0 left-0 w-full h-full -z-0 bg-black backdrop-blur-xl bg-opacity-70"></div>
         </div>
     )
