80e88064534de2e9ff8a3617ff9cf7a17f2a26f7
Luciano275 <luna800153@gmail.com>
2024-07-16 17:15:25 -0300

    🚀 Creando el calendario

--- client/app/(protected)/campus/page.tsx
+++ client/app/(protected)/campus/page.tsx
@@ -1,4 +1,6 @@
 import { auth } from "@/auth"
+import CalendarComponent from "@/components/ui/campus/Calendar";
+import { OpenNotifyButton } from "@/components/ui/campus/notify-buttons";
 import ThemeButton from "@/components/ui/public/theme-button";
 import { Metadata } from "next"
 
@@ -15,8 +17,13 @@ export default async function CampusPage() {
         <section className="min-h-screen relative overflow-x-hidden overflow-y-auto grow p-4">
             <header className={`w-full flex justify-between items-center pb-2 border-b border-base-300`}>
                 <h1 className="text-3xl">Campus virtual</h1>
-                <ThemeButton whiteColor />
+                <div className="flex items-center">
+                    <OpenNotifyButton />
+                    <ThemeButton whiteColor />
+                </div>
             </header>
+
+            <CalendarComponent />
         </section>
     )
 }
---
+++ client/components/ui/campus/Calendar.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { format, parse, startOfWeek, getDay } from 'date-fns'
+import { es } from 'date-fns/locale'
+import { useState } from 'react';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import '@/styles/calendar.css'
+
+export default function CalendarComponent() {
+
+    const [events, setEvents] = useState([
+        {
+            title: "Examen de Vila",
+            start: new Date(2024, 6, 30),
+            end: new Date(2024, 6, 30),
+            id: 2,
+        },
+        {
+            title: "Tarea de Mereles",
+            start: new Date(2024, 6, 31),
+            end: new Date(2024, 6, 31),
+            id: 3,
+        },
+        {
+            title: "Tarea de Fonseca",
+            start: new Date(2024, 6, 29),
+            end: new Date(2024, 6, 29),
+            id: 4,
+        },
+        {
+            title: "Lo de la Soto",
+            start: new Date(2024, 6, 25, 15, 40),
+            end: new Date(2024, 6, 25, 17, 10),
+            id: 5,
+        },
+        {
+            title: "Lo de la Soto",
+            start: new Date(2024, 6, 25, 15, 40),
+            end: new Date(2024, 6, 25, 17, 10),
+            id: 8,
+        },
+        {
+            title: "Lo de la Dina",
+            start: new Date(2024, 6, 25, 15, 0),
+            end: new Date(2024, 6, 25, 15, 40),
+            id: 6,
+        },
+    ])
+
+    const locales = {
+        'es': es
+    }
+
+    const localizer = dateFnsLocalizer({
+        format: format,
+        parse: parse,
+        startOfWeek: () => startOfWeek(new Date(), { weekStartsOn: 1 }),
+        getDay: getDay,
+        locales
+    })
+
+    return (
+      <section className='py-4'>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor={"start"}
+          endAccessor={"end"}
+          culture="es"
+          messages={{
+            next: "Siguiente",
+            previous: "Anterior",
+            today: "Hoy",
+            month: "Mes",
+            week: "Semana",
+            day: "Día",
+            agenda: "Agenda",
+            event: "Tarea",
+            noEventsInRange: "No hay tareas en este rango",
+            showMore: (total: number) => `+${total} más`,
+          }}
+          style={{
+            height: 600,
+          }}
+        />
+      </section>
+    );
+}
