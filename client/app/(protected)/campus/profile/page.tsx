0cd02822a1d413a9b9d2abab05c93193279f83ed
Luciano275 <luna800153@gmail.com>
2024-07-20 22:12:11 -0300

    Eliminando el diseño antiguo del perfil

--- client/app/(protected)/campus/profile/page.tsx
+++ client/app/(protected)/campus/profile/page.tsx
@@ -2,7 +2,6 @@ import { auth } from "@/auth";
 import CampusHeader from "@/components/ui/campus/Header";
 import Section from "@/components/ui/campus/Section";
 import { getUserByEmail } from "@/lib/user";
-import UserInfo from "@/components/ui/campus/profile/user-info";
 import UserCards from "@/components/ui/campus/profile/user-cards";
 
 export default async function ProfilePage(){
@@ -15,7 +14,6 @@ export default async function ProfilePage(){
       <Section>
         <CampusHeader title="Tu perfil" />
         <div className="flex pt-10 pb-4 md:px-10 gap-10 flex-wrap">
-          {/* <UserInfo user={user} rol={session?.user.rol!-1} /> */}
           <UserCards user={user} rol={session?.user.rol!-1} />
         </div>
       </Section>
