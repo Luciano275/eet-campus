d7582c52cb98307f7230284b2f3dac472e96cd82
Luciano275 <luna800153@gmail.com>
2024-08-08 23:59:18 -0300

    Agregado del redireccionamiento el LogoutButton

--- client/components/ui/campus/logout-button.tsx
+++ client/components/ui/campus/logout-button.tsx
@@ -1,27 +1,29 @@
-'use client';
+"use client";
 
 import { signOutAction } from "@/lib/authActions";
 import { BiLogOut } from "react-icons/bi";
 
-export default function LogoutButton () {
+export default function LogoutButton() {
+  const onClick = async () => {
+    await signOutAction();
+    window.location.reload();
+  };
 
-    const onClick = async () => {
-        await signOutAction();
-        window.location.href = '/'
-    }
-
-    return (
-      <div className="p-2" style={{
-        transition: 'box-shadow 500ms ease',
-        boxShadow: `0 -4px 15px 0 rgb(3 7 18)`
-      }}>
-        <button
-          className={`flex justify-center items-center gap-2 py-2 px-4 bg-gray-900 rounded-xl hover:bg-red-600 text-white w-full`}
-          onClick={onClick}
-        >
-          <BiLogOut size={20} />
-          Salir
-        </button>
-      </div>
-    );
+  return (
+    <div
+      className="p-2"
+      style={{
+        transition: "box-shadow 500ms ease",
+        boxShadow: `0 -4px 15px 0 rgb(3 7 18)`,
+      }}
+    >
+      <button
+        className={`flex justify-center items-center gap-2 py-2 px-4 bg-gray-900 rounded-xl hover:bg-red-600 text-white w-full`}
+        onClick={onClick}
+      >
+        <BiLogOut size={20} />
+        Salir
+      </button>
+    </div>
+  );
 }
--- client/lib/authActions.ts
+++ client/lib/authActions.ts
@@ -1,15 +1,18 @@
-'use server';
+"use server";
 
 import { signIn, signOut } from "@/auth";
 import { DEFAULT_REDIRECT } from "@/routes";
 
-export async function signInAction(provider: 'google' | 'github') {
-    await signIn(provider, {
-        redirect: true,
-        redirectTo: DEFAULT_REDIRECT
-    })
+export async function signInAction(provider: "google" | "github") {
+  await signIn(provider, {
+    redirect: true,
+    redirectTo: DEFAULT_REDIRECT,
+  });
 }
 
 export async function signOutAction() {
-    await signOut()
+  await signOut({
+    redirect: true,
+    redirectTo: "/",
+  });
 }
