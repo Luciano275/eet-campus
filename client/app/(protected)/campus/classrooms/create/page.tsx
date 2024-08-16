fe42bdcbfee6ad3047be0f334e69a2edad655c85
Luciano275 <luna800153@gmail.com>
2024-08-15 22:43:57 -0300

    🔧 Correción del bug del metadato "title" en la página de "Crear aulas"

--- client/app/(protected)/campus/classrooms/create/page.tsx
+++ client/app/(protected)/campus/classrooms/create/page.tsx
@@ -1,6 +1,15 @@
 import { auth } from "@/auth";
 import CreateClassroom from "@/components/pages/teacher/create/Page";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  const rol = (await auth())?.user.rol!;
+
+  return {
+    title: rol === 1 || rol === 2 ? 'Crear un aula' : 'Página no encontrada'
+  }
+}
 
 export default async function CreateClassroomPage() {
   
