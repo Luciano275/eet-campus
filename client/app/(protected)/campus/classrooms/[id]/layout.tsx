a5a218feb52651de6e56e35c5e3c8b78bc987605
Luciano275 <luna800153@gmail.com>
2024-08-17 19:22:25 -0300

    🔧 Correción del metadato del título en el aula

--- client/app/(protected)/campus/classrooms/[id]/layout.tsx
+++ client/app/(protected)/campus/classrooms/[id]/layout.tsx
@@ -2,7 +2,21 @@ import { auth } from "@/auth";
 import ClassroomMenu from "@/components/ui/campus/classrooms/ClassroomMenu";
 import CampusHeader from "@/components/ui/campus/Header";
 import { belongClassroom, findClassroomById } from "@/lib/classroom";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
+
+export const generateMetadata = async (
+  {params: { id }}
+  : {
+    params: { id: string }
+  }
+): Promise<Metadata> => {
+  const classroom = await findClassroomById(id);
+
+  return {
+    title: classroom ? classroom.name : 'Página no encontrada'
+  }
+}
 
 export default async function ClassroomLayout (
   {children, params: {id}}
