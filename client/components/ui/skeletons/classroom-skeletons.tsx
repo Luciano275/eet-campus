74be0f70b9f7bc759dc7d2007e09df3fe1decd43
Luciano275 <luna800153@gmail.com>
2024-08-17 18:15:18 -0300

    Creación de un skeleton para el input de los cursos

--- client/components/ui/skeletons/classroom-skeletons.tsx
+++ client/components/ui/skeletons/classroom-skeletons.tsx
@@ -20,3 +20,9 @@ export const ClassroomSkeleton = () => {
     </div>
   )
 }
+
+export const CoursesInputSkeleton = () => {
+  return (
+    <div className="h-10 rounded-lg skeleton"></div>
+  )
+}
