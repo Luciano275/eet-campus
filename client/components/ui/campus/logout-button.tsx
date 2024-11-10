27ab35f4767227b13ad10ceaf49a16b5b3bb8a51
Luciano275 <luna800153@gmail.com>
2024-11-10 19:03:24 -0300

    Uso del hook ActionState para el boton de Logout

--- client/components/ui/campus/logout-button.tsx
+++ client/components/ui/campus/logout-button.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import { signOutAction } from "@/lib/authActions";
+import { useActionState } from "react";
 import { BiLogOut } from "react-icons/bi";
+import Form from 'next/form'
 
 export default function LogoutButton() {
-  const onClick = async () => {
-    await signOutAction();
-    window.location.reload();
-  };
+  
+  const [state, action, isPending] = useActionState(signOutAction, undefined);
 
   return (
-    <div
+    <Form
+      action={action}
       className="p-2"
       style={{
         transition: "box-shadow 500ms ease",
@@ -18,12 +19,12 @@ export default function LogoutButton() {
       }}
     >
       <button
-        className={`flex justify-center items-center gap-2 py-2 px-4 bg-gray-900 rounded-xl hover:bg-red-600 text-white w-full`}
-        onClick={onClick}
+        className={`flex justify-center items-center gap-2 py-2 px-4 ${isPending ? 'bg-gray-950' : 'bg-gray-900 hover:bg-red-600'} rounded-xl text-white w-full`}
+        disabled={isPending}
       >
         <BiLogOut size={20} />
         Salir
       </button>
-    </div>
+    </Form>
   );
 }
