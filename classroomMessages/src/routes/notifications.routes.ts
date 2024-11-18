c3314964d2d99f790f169cc273acfecb8a311e9b
Luciano275 <luna800153@gmail.com>
2024-11-17 21:04:31 -0300

    Eliminacion del antiguo backend

--- classroomMessages/src/routes/notifications.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { NotificationController } from "../controllers/notification.controllers";
-
-const router = Router();
-
-router.get('/', NotificationController.getNotifications)
-router.post('/', NotificationController.createNotification)
-
-export default router;
