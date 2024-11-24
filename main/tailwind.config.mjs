0a502c0239e5d065e01af394c17e8583674fa35c
Luciano275 <luna800153@gmail.com>
2024-11-24 16:50:21 -0300

    Creacion de un Carousel de imagenes con Flowbite

--- main/tailwind.config.mjs
+++ main/tailwind.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from 'daisyui'
 import midudevAnimations from '@midudev/tailwind-animations'
+import flowbite from 'flowbite/plugin'
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./node_modules/flowbite/**/*.js"],
 	theme: {
 		extend: {},
 	},
-	plugins: [daisyui, midudevAnimations],
+	plugins: [daisyui, midudevAnimations, flowbite],
 }
