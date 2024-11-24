88ce80d5835432f2f5881c4d211f28a3102213a0
Luciano275 <luna800153@gmail.com>
2024-11-24 18:18:39 -0300

    Muestra de los blogs del colegio

--- main/tailwind.config.mjs
+++ main/tailwind.config.mjs
@@ -2,11 +2,12 @@
 import daisyui from 'daisyui'
 import midudevAnimations from '@midudev/tailwind-animations'
 import flowbite from 'flowbite/plugin'
+import tailwindTypography from '@tailwindcss/typography'
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./node_modules/flowbite/**/*.js"],
 	theme: {
 		extend: {},
 	},
-	plugins: [daisyui, midudevAnimations, flowbite],
+	plugins: [daisyui, midudevAnimations, flowbite, tailwindTypography],
 }
