373b2b72ff812f3604216270c0875995b358476f
Luciano275 <luna800153@gmail.com>
2024-12-21 02:19:08 -0300

    Eliminación del Socket Indicator

--- client/app/(protected)/campus/classrooms/[id]/page.tsx
+++ client/app/(protected)/campus/classrooms/[id]/page.tsx
@@ -24,8 +24,8 @@ export default async function ClassroomPage(
 
   return (
     <div className="p-4 border border-base-300 rounded-xl mx-auto flex flex-col gap-3">
-      <SocketIndicator />
-      <h2 className="text-2xl font-semibold">Acerca de esta aula</h2>
+      {/* <SocketIndicator /> */}
+      {/* <h2 className="text-2xl font-semibold">Acerca de esta aula</h2> */}
       {/* <p className="whitespace-pre-wrap">{classroom.description || 'No se agregó una descripción'}</p> */}
       
       { !classroom.description ? <p>No se agregó una descripción</p> : (
