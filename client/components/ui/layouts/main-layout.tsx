f6c2431be9b92970e0b51b96591e2cf9535d1507
Luciano275 <luna800153@gmail.com>
2024-12-22 00:15:42 -0300

    Correción de bug en la página NotFound

--- client/components/ui/layouts/main-layout.tsx
+++ client/components/ui/layouts/main-layout.tsx
@@ -4,15 +4,16 @@ import { usePathname, useSearchParams } from "next/navigation";
 import Header from "../public/Header";
 import AuthErrorPage from "@/components/errors/AuthError";
 import { ErrorEnums } from "@/types";
+import { Session } from "next-auth";
 
-export default function MainLayout({children}: {children: React.ReactNode}) {
+export default function MainLayout({children, session}: {children: React.ReactNode; session: Session | null}) {
 
     const pathname = usePathname();
     const searchParams = useSearchParams();
     const error = searchParams.get('error') as ErrorEnums || undefined;
 
     return (
-        pathname.includes('/campus') ? (
+        pathname.includes('/campus') || session ? (
             children
         ) : (
             <div className="flex flex-col min-h-screen">
