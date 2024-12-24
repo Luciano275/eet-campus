29ef14ba26262b33d5e60fe6c733ebadd3557e73
Luciano275 <luna800153@gmail.com>
2024-12-24 20:03:56 -0300

    Quitando el background solo en la ruta principal

--- main_v2/components/ui/Navbar.tsx
+++ main_v2/components/ui/Navbar.tsx
@@ -6,10 +6,12 @@ import NavbarButton from './navbar-button';
 import NavbarLinks from './navbar-links';
 import MobileNav from './MobileNav';
 import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
 
   const [scroll, setScroll] = useState(0);
+  const pathname = usePathname();
 
   useEffect(() => {
     setScroll(window.scrollY);
@@ -28,7 +30,7 @@ export default function Navbar() {
   return (
     <>
       <nav
-        className={`fixed top-0 left-0 w-full flex justify-between py-4 px-4 lg:px-8 items-center bg-opacity-90 ${scroll === 0 ? 'bg-transparent' : 'bg-blue-950'}`}
+        className={`fixed top-0 left-0 w-full flex justify-between py-4 px-4 lg:px-8 items-center bg-opacity-90 ${scroll === 0 && pathname === '/' ? 'bg-transparent' : 'bg-blue-950'}`}
         style={{ zIndex: 99999 }}
       >
         <div className="flex gap-5 items-center">
