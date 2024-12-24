90636c4aabf9a5cfbdd2edc70d068704956f1c3e
Luciano275 <luna800153@gmail.com>
2024-12-24 20:03:09 -0300

    Creación del background dinámico del Navbar

--- main_v2/components/ui/Navbar.tsx
+++ main_v2/components/ui/Navbar.tsx
@@ -5,12 +5,30 @@ import ThemeButton from '../ThemeButton';
 import NavbarButton from './navbar-button';
 import NavbarLinks from './navbar-links';
 import MobileNav from './MobileNav';
+import { useEffect, useState } from 'react';
 
 export default function Navbar() {
+
+  const [scroll, setScroll] = useState(0);
+
+  useEffect(() => {
+    setScroll(window.scrollY);
+
+    const handleScroll = () => {
+      setScroll(window.scrollY);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [])
+
   return (
     <>
       <nav
-        className={`fixed top-0 left-0 w-full flex justify-between py-4 px-4 lg:px-8 items-center bg-opacity-90 bg-blue-950`}
+        className={`fixed top-0 left-0 w-full flex justify-between py-4 px-4 lg:px-8 items-center bg-opacity-90 ${scroll === 0 ? 'bg-transparent' : 'bg-blue-950'}`}
         style={{ zIndex: 99999 }}
       >
         <div className="flex gap-5 items-center">
