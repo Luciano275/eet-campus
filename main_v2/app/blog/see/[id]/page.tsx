9288b21331c8ba02a2797a49f6cbe24623d31f17
Luciano275 <luna800153@gmail.com>
2024-12-26 16:10:58 -0300

    Agregar generación de metadatos dinámicos para la página del blog

--- main_v2/app/blog/see/[id]/page.tsx
+++ main_v2/app/blog/see/[id]/page.tsx
@@ -1,8 +1,40 @@
 import Section from "@/components/Section";
 import BlogInfoSkeleton from "@/components/skeletons/blog-info-skeleton";
 import Blog from "@/components/ui/blog/Blog";
+import { findBlog } from "@/lib/blogs";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
+
+export const generateMetadata = async (
+  {params}
+  : {
+    params: Promise<{
+      id: string;
+    }>
+  }
+): Promise<Metadata> => {
+  const id = (await params).id || null;
+
+  if (!id) {
+    return {
+      title: 'Blog no encontrado',
+    }
+  }
+
+  const blog = await findBlog(id);
+
+  if (!blog) {
+    return {
+      title: 'Blog no encontrado',
+    }
+  }
+
+  return {
+    title: blog.title,
+    description: blog.description,
+  }
+}
 
 export default async function SeeBlogPage(
   { params }
