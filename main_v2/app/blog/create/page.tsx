7f7abaf7e6fdd1b8505c53852f95c4bc6ba06e43
Luciano275 <luna800153@gmail.com>
2024-12-26 16:46:32 -0300

    Agregar lógica de autenticación en la página de creación de blogs

--- main_v2/app/blog/create/page.tsx
+++ main_v2/app/blog/create/page.tsx
@@ -2,8 +2,21 @@ import BlogProvider from "@/components/providers/blog-provider";
 import Section from "@/components/Section";
 import CreateBlogForm from "@/components/ui/blog/create/Form";
 import CreateBlogTabs from "@/components/ui/blog/create/Tabs";
+import { auth } from "@/lib/user";
+import { notFound } from "next/navigation";
 
-export default function CreateBlogPage() {
+export default async function CreateBlogPage() {
+
+  const authResult = await auth();
+
+  if (!authResult.success) {
+    throw new Error(authResult.error);
+  }
+
+  if (!authResult.session) {
+    notFound();
+  }
+
   return (
     <BlogProvider>
       <Section>
