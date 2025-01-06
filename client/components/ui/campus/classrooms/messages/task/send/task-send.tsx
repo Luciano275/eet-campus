25b633b5019aace70f759dc882cb83b9f8b0e068
Luciano275 <luna800153@gmail.com>
2025-01-05 22:37:46 -0300

    Ajustar estilos en el componente TaskSend para mejorar la consistencia visual en el modo oscuro

--- client/components/ui/campus/classrooms/messages/task/send/task-send.tsx
+++ client/components/ui/campus/classrooms/messages/task/send/task-send.tsx
@@ -11,7 +11,7 @@ export default function TaskSend(
 ) {
   if (availableToSend && (rol === 1 || rol === 3)) {
     return (
-      <div className="w-full max-w-[300px] flex flex-col gap-4 lg:max-w-[400px] p-4 border border-base-300 dark:border-neutral-700 rounded-md">
+      <div className="w-full max-w-[300px] flex flex-col gap-4 lg:max-w-[400px] p-4 border border-base-300 dark:border-base-300 rounded-md dark:bg-base-200">
         <h2 className="text-2xl pt-2 pb-4 border-b border-base-300 dark:border-neutral-700">Entregar tarea</h2>
 
         <TaskSendForm owner={owner} />
