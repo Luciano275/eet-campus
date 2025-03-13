0e9d063a79728875082a04ceec85571a764822b8
Luciano275 <luna800153@gmail.com>
2025-03-13 15:48:44 -0300

    Refactorizar el componente SignInButton: extraer la lógica del botón en un componente Button reutilizable y mejorar las transiciones de estilo

--- client/components/ui/signin-button.tsx
+++ client/components/ui/signin-button.tsx
@@ -3,44 +3,103 @@
 import { signInAction } from "@/lib/authActions";
 import { useActionState, useEffect, useState } from "react";
 
-export default function SignInButton() {
+const Button = (
+  {children, className, pending}
+  : {
+    children: React.ReactNode;
+    className: string;
+    pending: boolean;
+  }
+) => {
+  return (
+    <button
+      style={{ transition: "scale .2s ease, background .2s ease" }}
+      disabled={pending}
+      className={className}
+    >
+      {children}
+    </button>
+  )
+}
 
-  const className = 'flex w-full items-center gap-2 py-3 px-6 rounded-xl transition-transform hover:scale-105'
+export default function SignInButton() {
+  const className =
+    "flex w-full items-center gap-2 py-3 px-6 rounded-xl hover:scale-105";
 
   const [pending, setPending] = useState(false);
 
-  const bindGoogleAction = signInAction.bind(null, 'google');
-  const bindGithubAction = signInAction.bind(null, 'github');
+  const bindGoogleAction = signInAction.bind(null, "google");
+  const bindGithubAction = signInAction.bind(null, "github");
 
-  const [stateGoogle, googleAction, isPendingGoogle] = useActionState(bindGoogleAction, undefined);
-  const [stateGithub, githubAction, isPendingGithub] = useActionState(bindGithubAction, undefined);
+  const [stateGoogle, googleAction, isPendingGoogle] = useActionState(
+    bindGoogleAction,
+    undefined
+  );
+  const [stateGithub, githubAction, isPendingGithub] = useActionState(
+    bindGithubAction,
+    undefined
+  );
 
   useEffect(() => {
     setPending(isPendingGoogle || isPendingGithub);
-  }, [isPendingGithub, isPendingGoogle])
+  }, [isPendingGithub, isPendingGoogle]);
 
   return (
     <div className="flex flex-col w-full max-w-[300px] gap-4 mt-5 border-t py-10 border-base-300">
       <form action={googleAction}>
-        <button disabled={pending} className={`${className} text-black ${pending ? 'contrast-50' : 'bg-neutral-100 hover:bg-neutral-200'}`}>
+        <Button
+          pending={pending}
+          className={`${className} text-black ${
+            pending ? "contrast-50" : "bg-neutral-100 hover:bg-neutral-200"
+          }`}
+        >
           <span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" preserveAspectRatio="xMidYMid" viewBox="0 0 256 262" id="google"><path fill="#4285F4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"></path><path fill="#34A853" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"></path><path fill="#FBBC05" d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"></path><path fill="#EB4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"></path></svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              preserveAspectRatio="xMidYMid"
+              viewBox="0 0 256 262"
+              id="google"
+            >
+              <path
+                fill="#4285F4"
+                d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
+              ></path>
+              <path
+                fill="#34A853"
+                d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
+              ></path>
+              <path
+                fill="#FBBC05"
+                d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"
+              ></path>
+              <path
+                fill="#EB4335"
+                d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
+              ></path>
+            </svg>
           </span>
-          <span className="grow">
-            Continuar con Google
-          </span>
-        </button>
+          <span className="grow">Continuar con Google</span>
+        </Button>
       </form>
       <form action={githubAction}>
-        <button disabled={pending} className={`${className} ${pending ? 'contrast-50' : 'bg-neutral-800 hover:bg-neutral-900'} text-white`}>
+        <Button
+          pending={pending}
+          className={`${className} ${
+            pending ? "contrast-50" : "bg-neutral-800 hover:bg-neutral-900"
+          } text-white`}
+        >
           <span>
-            <img src={'/assets/github.svg'} className="w-full max-w-[25px] h-auto" alt="GitHub Logo" />
+            <img
+              src={"/assets/github.svg"}
+              className="w-full max-w-[25px] h-auto"
+              alt="GitHub Logo"
+            />
           </span>
-          <span className="grow">
-            Continuar con GitHub
-          </span>
-        </button>
+          <span className="grow">Continuar con GitHub</span>
+        </Button>
       </form>
     </div>
-  )
+  );
 }
